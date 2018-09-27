9e13f025d22f997134bab6f6128f90fada10526b
Yuliia Shypat <juliashipat@gmail.com>
2018-09-27 12:49:17 +0200

    Firebase configuration. Getting first collection

--- client-panel/src/environments/environment.prod.ts
+++ client-panel/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDYYcurL8z6nu5DDB0nU7tr-m6_E5cecKs',
+    authDomain: 'client-panel-prod-51c58.firebaseapp.com',
+    databaseURL: 'https://client-panel-prod-51c58.firebaseio.com',
+    projectId: 'client-panel-prod-51c58',
+    storageBucket: 'client-panel-prod-51c58.appspot.com',
+    messagingSenderId: '362058961625'
+  }
 };
--- client-panel/src/environments/environment.ts
+++ client-panel/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDYYcurL8z6nu5DDB0nU7tr-m6_E5cecKs',
+    authDomain: 'client-panel-prod-51c58.firebaseapp.com',
+    databaseURL: 'https://client-panel-prod-51c58.firebaseio.com',
+    projectId: 'client-panel-prod-51c58',
+    storageBucket: 'client-panel-prod-51c58.appspot.com',
+    messagingSenderId: '362058961625'
+  }
 };
