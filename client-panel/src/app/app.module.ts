26372570c3eba554f37f0cf9ff92ce943a2c4efa
Julia Shypat <juliashipat@gmail.com>
2018-09-30 12:00:32 +0200

    New client form and validation

--- client-panel/src/app/app.module.ts
+++ client-panel/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AngularFireModule} from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppRoutingModule } from './app-routing.module';
-
+import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
@@ -41,7 +41,8 @@ import { ClientService } from './services/client.service';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase, 'clientpanel'),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    FormsModule
   ],
   providers: [ClientService],
   bootstrap: [AppComponent]
