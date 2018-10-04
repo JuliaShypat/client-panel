bcf388cdc768dd8e7c8ed975f0477e24f9ffbd8d
Yuliia Shypat <juliashipat@gmail.com>
2018-10-04 16:26:24 +0200

    Get client deails and show on details page

--- client-panel/src/app/components/client-details/client-details.component.ts
+++ client-panel/src/app/components/client-details/client-details.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/Client';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 @Component({
   selector: 'app-client-details',
   templateUrl: './client-details.component.html',
   styleUrls: ['./client-details.component.css']
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance: boolean;
+  showBalanceUpdateInput: Boolean = false;
 
-  constructor() { }
+  constructor(private clientService: ClientService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.clientService.getClient(this.id).subscribe(client => {
+      if (client !== null) {
+        if (client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+      this.client = client;
+    });
   }
 
 }
